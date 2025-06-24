fc1eeabc363c719033c17f308375aecd3f8b3374
Ofir Porat <solutionsbyoli@gmail.com>
2025-06-24 09:31:20 -0700

    update david star sb350 script

---
+++ companies/david-star/sb350.js
@@ -0,0 +1,7 @@
+import { updateSB350Base } from "../../templates/sb350.js";
+import { companyInfo } from "./company-info.js";
+import { serializeFunction } from "../../utils/serializeFunction.js";
+
+const renderedScript = serializeFunction(updateSB350Base, companyInfo);
+
+export default renderedScript;
