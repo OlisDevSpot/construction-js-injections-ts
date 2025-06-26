42979a8a57bb6f384c8ec995fa6cb36cc20289e4
Olis Solutions <solutionsbyoli@gmail.com>
2025-06-26 15:35:37 -0700

    update yelp template with {{reviews}}

--- utils/company.js
+++ utils/company.js
@@ -1,13 +1,4 @@
 import { companies } from "../companies";
-import { reviews } from "../constants/reviews";
-
-export function generateCompany(companyMetadata, type) {
-  return {
-    ...companyMetadata,
-    type,
-    relevantReviews: reviews[type],
-  };
-}
 
 export function getCompany(companyName) {
   return companies[companyName];
