04327ab17c9abd780a5c677182dac9888c5af280
Ofir Porat <solutionsbyoli@gmail.com>
2025-06-26 12:19:31 -0700

    update all in 1 aliases

--- companies/all-in-1/company-info.js
+++ companies/all-in-1/company-info.js
@@ -1,7 +1,7 @@
 export const allIn1 = {
   key: "all-in-1",
   name: "All In 1 Remodeling",
-  aliases: ["All In 1", "All In One"],
+  aliases: ["All In 1", "All In One", "BTV Construction"],
   link: "https://www.allin1remodeling.com",
   licenseNum: "1107796",
 };
