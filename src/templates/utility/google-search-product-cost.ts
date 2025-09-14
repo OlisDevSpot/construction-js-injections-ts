c8679354d3de6f0a1a7d8f73e8bdec80bde71f53
Oliver P <solutionsbyoli@gmail.com>
2025-09-14 10:28:06 -0700

    testing outside consts

--- src/templates/utility/google-search-product-cost.ts
+++ src/templates/utility/google-search-product-cost.ts
@@ -1,9 +1,12 @@
 import { ScriptTemplate } from "@/types/templates";
 
+const injected = "OLIVER IS COOL!";
+
 export function templateFn() {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
   const query = params.get("q")!.toLowerCase();
+  console.log(injected);
 
   const products = {
     artificial: {
